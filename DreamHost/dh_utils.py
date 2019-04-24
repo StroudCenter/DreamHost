725ae100ad4b9c0f01bf867e086c2a5e79bde78d
Sara Damiano <sdamiano@stroudcenter.org>
2019-04-24 15:30:47 -0400

    Parenthesis error

--- DreamHost/dh_utils.py
+++ DreamHost/dh_utils.py
@@ -326,7 +326,7 @@ def get_data_from_dreamhost_table(table, column, start_dt=None, end_dt=None, deb
 
         # estimate what we should be correcting by
         values_table['server_offset'] = (values_table['server_timestamp'].dt.tz_convert(tz="Etc/GMT+5") -
-                                         values_table['timestamp']).dt.tz_convert(tz="Etc/GMT+5")
+                                         values_table['timestamp'].dt.tz_convert(tz="Etc/GMT+5"))
         values_table['server_offset_round'] = values_table['server_offset'].dt.floor(freq='5min')
 
         # don't correct if the needed correction would be less than 5 minutes
